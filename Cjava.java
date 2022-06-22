34695ff9e5afe4939af9f48fce66b7c6b94943fc
UDAY9337 <106107028+UDAY9337@users.noreply.github.com>
2022-06-22 13:54:45 +0530

    Add files via upload

---
+++ Cjava.java
@@ -0,0 +1,9 @@
+class Cjava
+{
+  public static void main(String[]args)
+  {
+    System.out.println("Name : uday dutta");
+	System.out.println("Course : Java Development");
+	System.out.println("Stream : bca");
+  }
+}
